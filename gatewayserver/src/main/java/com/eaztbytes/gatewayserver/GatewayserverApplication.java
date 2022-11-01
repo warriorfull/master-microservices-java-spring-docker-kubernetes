fc1058f2288f006433586d9865172051b0c46fc3
Todd Eric Whitehead <warriorfullarmorofgod@gmail>
2022-11-01 11:19:56 -0500

    87. Impletmenting Custom Routing using Spring Cloud Gateway

--- gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
@@ -1,8 +1,13 @@
 package com.eaztbytes.gatewayserver;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +15,27 @@ public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p -> p
+					.path("/eaztbank/accounts/**")
+					.filters(f -> f.rewritePath("/eaztbank/accounts/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", new Date().toString()))
+					.uri("lb://ACCOUNTS"))
+			.route(p -> p
+					.path("/eaztbank/loans/**")
+					.filters(f -> f.rewritePath("/eaztbank/loans/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", new Date().toString()))
+					.uri("lb://LOANS"))
+			.route(p -> p
+					.path("/eaztbank/cards/**")
+					.filters(f -> f.rewritePath("/eaztbank/cards/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", new Date().toString()))
+					.uri("lb://CARDS"))
+			.build();
 	}
 
 }
